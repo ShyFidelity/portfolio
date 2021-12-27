1832e10b46f22fa238ffd9ccb55f2097a7117c1f
jkworks <info@jilliankayworks.com>
2021-12-27 14:38:06 -0700

    copied all contents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { lazy, Suspense } from "react";
+import GlobalStyle from "./globalStyles";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import Loading from "./subComponents/Loading";
+
+//Components
+const Main = lazy(() => import("./components/Main"));
+const AboutPage = lazy(() => import("./components/AboutPage"));
+const MySkillsPage = lazy(() => import("./components/MySkillsPage"));
+const BlogPage = lazy(() => import("./components/BlogPage"));
+const WorkPage = lazy(() => import("./components/WorkPage"));
+const SoundBar = lazy(() => import("./subComponents/SoundBar"));
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        <Suspense fallback={<Loading />}>
+          <SoundBar />
+
+          <AnimatePresence exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route exact path="/" component={Main} />
+
+              <Route exact path="/about" component={AboutPage} />
+
+              <Route exact path="/blog" component={BlogPage} />
+
+              <Route exact path="/work" component={WorkPage} />
+
+              <Route exact path="/skills" component={MySkillsPage} />
+            </Switch>
+          </AnimatePresence>
+        </Suspense>
+      </ThemeProvider>
+    </>
   );
 }
 
