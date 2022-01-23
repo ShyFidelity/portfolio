a4317b70d2a61466799ad2f19d131b68eae0890f
jkworks <info@jilliankayworks.com>
2022-01-23 15:06:20 -0700

    added art data

--- src/components/ArtDesignData.js
+++ src/components/ArtDesignData.js
@@ -1,6 +1,10 @@
 import Rattlesnake from '../assets/Images/rattlesnake.png'
 import Saturn from '../assets/Images/saturn.png'
 import SnakeFlower from '../assets/Images/snakeflower.png'
+import Curious from '../assets/Images/curious.png'
+import JustRelax from '../assets/Images/justrelax.png'
+import Phone from '../assets/Images/phoneafriend.png'
+import NeverLive from '../assets/Images/icouldneverlivewithoutyousmaller.png'
 
 
 export const ArtDesignData = [
@@ -24,4 +28,34 @@ export const ArtDesignData = [
         img: Saturn
         
     },
+
+    {
+        id:4,
+        name:"Friends of Toto",
+        img: Curious
+        
+    },
+
+    {
+        id:5,
+        name:"Friends of Toto",
+        img: JustRelax
+        
+    },
+
+    {
+        id:6,
+        name:"Friends of Toto",
+        img: NeverLive
+        
+    },
+
+    {
+        id:7,
+        name:"Friends of Toto",
+        img: Phone
+        
+    },
+
 ]
+
